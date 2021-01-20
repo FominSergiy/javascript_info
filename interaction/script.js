74f92f198ea8a69a71182f2052ed12c03951df12
Sergiy Fomin <sergiy.fomin@sourcedgroup.com>
2021-01-20 18:25:37 -0500

    Interaction: alert, prompt, confirm

---
+++ interaction/script.js
@@ -0,0 +1,5 @@
+let name = prompt('Input your name', '');
+let confirmed = confirm(`Are you sure your name is ${name}?`);
+alert(confirmed ? `good on you, you know your name, ${name}`
+    : 'I guess you can\'t spell your name right');
+
