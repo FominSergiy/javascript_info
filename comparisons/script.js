68362217939806af04531cdaa2ee2d955c6c3f19
Sergiy Fomin <sergiy.fomin@sourcedgroup.com>
2021-01-21 17:55:23 -0500

    comparisons

---
+++ comparisons/script.js
@@ -0,0 +1,9 @@
+// TODO Comparisons
+
+5 > 4 //? True
+"apple" > "pineapple" //? False
+"2" > "12"  //! False -- true because two strings are compared
+undefined == null //? True
+undefined === null //? False
+null == "\n0\n" //? false
+null === +"\n0\n" //? false
