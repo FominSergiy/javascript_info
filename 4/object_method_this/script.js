2488a1ccc2c2c6b22a0e4536f0f0b1be72559477
Sergiy Fomin <sergiy.fomin@sourcedgroup.com>
2021-01-26 20:47:56 -0500

    Object methods, this

--- 4/object_method_this/script.js
+++ 4/object_method_this/script.js
@@ -4,8 +4,8 @@ let calculator = {
     a: 0,
     b: 0,
     read() {
-        let a = prompt("Enter value for a ", "");
-        let b = prompt("Enter value for b ", "");
+        let a = +prompt("Enter value for a ", "");
+        let b = +prompt("Enter value for b ", "");
         this.a = a;
         this.b = b;
     },
@@ -21,3 +21,23 @@ calculator.read();
 alert(calculator.sum());
 alert(calculator.mul());
 alert(`val for a ${calculator.a}, and ${calculator.b} for b`);
+
+
+//TODO Chaining
+
+let ladder = {
+    sztep: 0,
+    up() {
+        this.step++;
+        return this;
+    },
+    down() {
+        this.step--;
+        return this;
+    },
+    showStep() { // shows the current step
+        alert(this.step);
+    }
+};
+
+ladder.up().up().showStep();
